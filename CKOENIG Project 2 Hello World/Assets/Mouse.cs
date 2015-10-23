7da24c6b212379e313a50ab5fc630fa38b736fa3
CK4003 <koenigc@coyote.csusb.edu>
2015-10-22 21:28:19 -0700

    Added Project 2 Hello World

---
+++ CKOENIG Project 2 Hello World/Assets/Mouse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("1", typeof(Sprite)) as Sprite;
+        }
+       
+    }
+
+    void OnMouseDown ()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("1", typeof(Sprite)) as Sprite;
+    }
+
+
+}
---
+++ CKOENIG Project 2 Hello World/Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if(Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, speed);
+        }
+    }
+}
