1d1afc42eb40eca95159c9f078123dc83e4dc174
CK4003 <koenigc@coyote.csusb.edu>
2015-10-29 12:00:36 -0700

    Added CKOENIG Project 3 Game World

---
+++ CKOENIG Project 3 Game World/Assets/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown("up"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300f);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, speed);
+        }
+
+        
+    }
+}
